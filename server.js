a0f547645e43273d841e1e07d88460949c8ad921
junesung1004 <junecoding1004@gmail.com>
2024-12-09 14:53:01 +0900

    feat : cors 설정 추가

--- server.js
+++ server.js
@@ -5,11 +5,10 @@
 // npm i mongoose
 // npm i dotenv
 
-//testaddasdadadaddad
-
 const express = require("express");
 const connectDB = require("./config/db.js");
 require("dotenv").config();
+const cors = require("cors");
 const userRouter = require("./routes/userRouter.js");
 const mentorRouter = require("./routes/mentorRouter.js");
 
@@ -17,6 +16,17 @@ const app = express();
 
 // JSON 바디 파서 미들웨어
 app.use(express.json());
+
+// cors 설정
+// CORS 설정
+app.use(
+  cors({
+    origin: "http://localhost:3000", // 허용할 도메인
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // 허용할 HTTP 메소드
+    allowedHeaders: ["Authorization", "Content-Type"], // 허용할 헤더
+    credentials: true, // 쿠키 전송 허용
+  })
+);
 
 // 라우터 연결
 app.use("/users", userRouter);
