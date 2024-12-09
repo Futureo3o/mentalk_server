2a5322ec321f32bece35ffb0c4dcf4e1440f922e
junesung1004 <junecoding1004@gmail.com>
2024-12-09 17:50:14 +0900

    feat: 멘티 관련 기능 추가

--- routes/mentorRouter.js
+++ routes/mentorRouter.js
@@ -2,11 +2,10 @@ const express = require("express");
 const mentorController = require("../controllers/mentorController");
 const router = express.Router();
 
-//멘토 회원가입 정보 관련 라우터 설정
-router.post("/signup", mentorController.createMentorUser);
+//멘토 정보 관련 라우터 설정
 router.get("/", mentorController.getAllMentorUser);
-router.get("/:id", mentorController.getMentorUserById);
-router.put("/:id", mentorController.updateMentorUser);
-router.delete("/:id", mentorController.deleteMentorUser);
+router.get("/:mentor_id", mentorController.getMentorUserById);
+router.put("/:mentor_id", mentorController.updateMentorUser);
+router.delete("/:mentor_id", mentorController.deleteMentorUser);
 
 module.exports = router;
