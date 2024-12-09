b990e82b391e9a52f2c8a068ccf9940849f1d25a
junesung1004 <junecoding1004@gmail.com>
2024-12-09 11:00:21 +0900

    git merge한 코드를 dev브랜치에서 가져오는데 왜 충돌이 안나지?

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-// npm init
-// npm i express
-// npm i nodemon --save-dev
-// npm i mongodb  --save
-// npm i mongoose
-// npm i dotenv....
+const { DatabaseSync } = require("node:sqlite")
 
-const express = require("express");
-const connectDB = require("./config/db.js");
-require("dotenv").config();
-const userRouter = require("./routes/userRouter.js");
-const mentorRouter = require("./routes/mentorRouter.js");
-
-const app = express();
-
-// JSON 바디 파서 미들웨어
-app.use(express.json());
-
-// 라우터 연결
-app.use("/users", userRouter);
-app.use("/mentor", mentorRouter);
-
-app.listen(process.env.PORT, () => {
-  console.log(`${process.env.PORT}번 포트에asdasdadsad....`);
-  console.log(`${prasdadasdT}번 포트에서 서버가 실행 중../..sadasdasdasdsadsadsadsadsad....`);
-  console.log(`${process.env.PORT}번 포asdadassadsadsad....`);
-  connectDB();
-});
-
-app.get("/", (req, res) => {
-  res.send("서버 접속 성공");
-});
+sadaskdjasldkajsdkldjasd
+aslkdjaslkdjasldjaljdad'
+asd
+askdjsaldjalkdjlsajdlajdq
+2k12kdla
+;
+setDefaultAutoSelectFamily;
+as;d
+;2
+1;else
+;ADDRCONFIGA;
+D;AS
+D;
+SafeArrayDAS
+DatabaseSyncD
+2
+W
+12
+3
+queueMicrotaskD
+ASD
+askdjsaldjalkdjlsajdlajdqDAS
+D
