39cbfc8fd23d9e8ea69742475ca9d2a2782bd1d3
parkhasung <kushiroxy@gmail.com>
2024-12-10 16:00:28 +0900

    feat: 디테일 페이지요청

--- controllers/mentorIntroduceController.js
+++ controllers/mentorIntroduceController.js
@@ -4,62 +4,26 @@ const MentorIntroduce = require("../models/mentorIntroduce");
         //URL 요청 받음
         const {mentor_id} = req.params;        
         try{
-            const mentor_intro = await MentorIntroduce.findOne({mentor_id :mentor_id});
+            const mentor_intro = await MentorIntroduce.findOne({mentor_id});
             
             if(!mentor_intro){
-                return res.status(404).json({error : '멘토 아이디 조회 실패'});
+                return res.status(404).json({error : '멘토 아이디 조회 실패',     
+                    message: '멘토 정보가 없습니다. 글 작성을 진행해주세요.',
+                });
             }
 
-            res.json(mentor_intro);
+            res.json({
+                mentor_id: mentor_intro.mentor_id,
+                mentor_title: mentor_intro.mentor_title,
+                mentor_content: mentor_intro.mentor_content,
+                review_count : mentor_intro.review_count,
+                coffeechat_count:mentor_intro.coffeechat_count,
+                 mentor_rating:mentor_intro.mentor_rating,
+        });
         }catch(error){
             res.status(500).json({error:"멘토포스팅 조회 중 오류가 발생했습니다."});
         }
     };
 
-    const createUser = async(req,res) => {
-        try {
-            const {mentor_id, mentor_title, mentor_content} = req.body
-            const newMentor = new MentorIntroduce({mentor_id, mentor_title, mentor_content})
-            const saveUser = await newMentor.save()
-            res.status(201).json({
-                message: "멘토 포스팅 작성 성공",
-                data: saveUser,
-              });
-              console.log("멘토 포스팅 작성 성공");
-        }catch (error) {
-            console.error("멘토 포스팅 작성 실패 : ", error);
-            res.status(500).json({ error: "포스팅을 생성하는 도중 오류가 발생했습니다." });
-          }
-        
 
-    }
-
-    //전부다 불러옴
-    // const getMentorAllIntroduce = async(req, res) => {
-    //     try {
-    //         const user = await MentorIntroduce.find()
-
-    //         if(!user) {
-    //             return res.status(500).json({error: "실패"})
-    //         }
-    //         res.json(user)
-    //     } catch(error) {
-    //         console.error("에러")
-    //     }
-    // }
-
-    // const createMentorIntroduce =async(req,res)=>{
-    //     try{
-    //         const{mentor_title,mentor_content}=req.body;
-    //         const newintro = new MentorIntroduce({mentor_title,mentor_content});
-
-    //         const savedIntro=await newintro.save();
-    //         res.status(201).json({message:'포스트가 성공적으로 생성되었습니다.',post:savedIntro});
-    //     }catch(error){
-    //         console.error('포스트 생성 실패',error);
-    //         res.status(500).json({error:'포스트 생성 중 오류가 발생했습니다.'});
-    //     }
-    // };
-    
 module.exports = { getMentorIntroduce, createUser };
-//createMentorIntroduce
--- models/mentorIntroduce.js
+++ models/mentorIntroduce.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const mentorIntroduceSchema = new mongoose.Schema(
     {
-      mentor_id:{type: String , required:true},
+      mentor_id:{type: String , ref: "Mentor",required:true},
       mentor_title: { type : String , required: true },
       mentor_content : {type : String, required: true},
-
+      review_count : {type:Number, default: 0},
+      coffeechat_count:{type:Number,default:0},
+      mentor_rating:{type:Number,default:0},
     },
     { timestamps: true }
   );
