0fc0680bf8ed15f8f33822b80f73bed4ee4cbc6d
parkhasung <kushiroxy@gmail.com>
2024-12-10 12:04:07 +0900

    feat: 멘토인트로 조회완료

--- controllers/mentorIntroduceController.js
+++ controllers/mentorIntroduceController.js
@@ -2,12 +2,9 @@ const MentorIntroduce = require("../models/mentorIntroduce");
 //버튼 클릭 시 화면
     const getMentorIntroduce = async(req,res)=>{
         //URL 요청 받음
-        const {mentor_id} = req.params;
-        console.log('Received mentor_id:', mentor_id);
-        
+        const {mentor_id} = req.params;        
         try{
-            const mentor_intro = await MentorIntroduce.findOne({mentor_id });
-            console.log('Received mentor_id:', mentor_intro);
+            const mentor_intro = await MentorIntroduce.findOne({mentor_id :mentor_id});
             
             if(!mentor_intro){
                 return res.status(404).json({error : '멘토 아이디 조회 실패'});
@@ -25,30 +22,31 @@ const MentorIntroduce = require("../models/mentorIntroduce");
             const newMentor = new MentorIntroduce({mentor_id, mentor_title, mentor_content})
             const saveUser = await newMentor.save()
             res.status(201).json({
-                message: "유저 생성 성공",
+                message: "멘토 포스팅 작성 성공",
                 data: saveUser,
               });
-              console.log("유저 생성 성공");
+              console.log("멘토 포스팅 작성 성공");
         }catch (error) {
-            console.error("유저 생성 실패 : ", error);
-            res.status(500).json({ error: "유저를 생성하는 도중 오류가 발생했습니다." });
+            console.error("멘토 포스팅 작성 실패 : ", error);
+            res.status(500).json({ error: "포스팅을 생성하는 도중 오류가 발생했습니다." });
           }
         
 
     }
 
-    const getMentorAllIntroduce = async(req, res) => {
-        try {
-            const user = await MentorIntroduce.find()
+    //전부다 불러옴
+    // const getMentorAllIntroduce = async(req, res) => {
+    //     try {
+    //         const user = await MentorIntroduce.find()
 
-            if(!user) {
-                return res.status(500).json({error: "실패"})
-            }
-            res.json(user)
-        } catch(error) {
-            console.error("에러")
-        }
-    }
+    //         if(!user) {
+    //             return res.status(500).json({error: "실패"})
+    //         }
+    //         res.json(user)
+    //     } catch(error) {
+    //         console.error("에러")
+    //     }
+    // }
 
     // const createMentorIntroduce =async(req,res)=>{
     //     try{
@@ -63,5 +61,5 @@ const MentorIntroduce = require("../models/mentorIntroduce");
     //     }
     // };
     
-module.exports = { getMentorIntroduce, getMentorAllIntroduce, createUser };
+module.exports = { getMentorIntroduce, createUser };
 //createMentorIntroduce
