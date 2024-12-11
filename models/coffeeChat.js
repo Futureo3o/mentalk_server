7f6a704d8989a43a7f97eeb3c19d21223c76cd08
junesung1004 <junecoding1004@gmail.com>
2024-12-11 16:31:26 +0900

    fix: 커피챗 스키마 속성 수정

--- models/coffeeChat.js
+++ models/coffeeChat.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const coffeeChatSchema = new mongoose.Schema(
   {
-    mentor_intro_unique_id: {
+    introduce_id: {
       type: mongoose.Schema.Types.ObjectId, // ObjectId로 설정
       ref: "mentorIntroduce", // mentorIntroduce 컬렉션을 참조
       required: true,
@@ -12,7 +12,7 @@ const coffeeChatSchema = new mongoose.Schema(
     coffee_status: { type: String, default: "신청" },
     coffee_cancle: { type: String, default: null },
     coffee_request: { type: String, required: true },
-    coffee_wanted: { type: String, required: true },
+    coffee_wanted: { type: [String], required: true },
   },
   { timestamps: true }
 );
