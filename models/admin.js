a7401216433be0ec7272dc5e202a2cf75f43cb45
junesung1004 <junecoding1004@gmail.com>
2024-12-16 17:28:59 +0900

    fix: mentee update할때 이미지를 계속 값이 바뀌니 const로 변수 선언이 아닌 let으로 값을 바꿀 수 있게 적용해야한다.

--- models/admin.js
+++ models/admin.js
@@ -6,7 +6,11 @@ const adminSchema = new mongoose.Schema({
   admin_pw: { type: String, required: true },
   admin_phone: { type: Number, required: true },
   admin_email: { type: String, required: true },
-  admin_access: { type: [String], requird: true },
+  admin_access: {
+    type: [String],
+    required: true,
+    enum: ["모든권한", "멘토관리", "멘티관리", "재직증명서관리", "정지기능능"],
+  },
 });
 
 const Admin = mongoose.model("Admin", adminSchema);
